fa16bef9b39881a4af8678d0148638756816082e
ManavRamani17 <manavramani2384@gmail.com>
2026-07-29 14:56:44 +0530

    student link add

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -8,7 +8,7 @@ function Header() {
         <Link className='text-xl m-3 transition-all hover:scale-110 duration-500' href="/About" scroll={true}>About</Link>    
         <Link className='text-xl m-3 transition-all hover:scale-110 duration-500' href="/Contact">Contact</Link>
         <Link className='text-xl m-3 transition-all hover:scale-110 duration-500' href="/College">College</Link>
-       
+        <Link className='text-xl m-3 transition-all hover:scale-110 duration-500' href="/Students">Students</Link>
     </div>
   )
 }

f1ac85ec6144e6ea2f060d809cb43a79e179da58
ManavRamani17 <manavramani2384@gmail.com>
2026-08-11 12:22:26 +0530

    updated

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div>
-        <h1 className='text-3xl text-center m-5'>Welcome to Dashboard</h1>
+        <h1 className='text-3xl text-center m-5 text-white transform hover:scale-105 duration-500'>Welcome to Dashboard</h1>
     </div>
   )
 }
